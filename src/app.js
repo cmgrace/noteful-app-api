3d2cf439851b5ea35873fd28d01dfcecc447c50b
Ming Cheng <cmgrace@bu.edu>
2021-03-08 21:20:08 -0500

    revise

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(morgan(morganOption));
 app.use(helmet());
 //app.use(cors());
-app.use(cors({ origin: "https://noteful-app-client.cmgrace.vercel.app" }));
+app.use(cors({ origin: "https://noteful-app-client-cmgrace.vercel.app" }));
 
 app.use("/api", folderRouter);
 app.use("/api", noteRouter);
